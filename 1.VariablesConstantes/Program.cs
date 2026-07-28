d58f6cd229f0f09dd1ca2764ac7945ec059d9468
Juanmaa07 <juan.castanoz@upb.edu.co>
2026-07-28 13:38:07 -0500

    Mi primer commit

---
+++ 1.VariablesConstantes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace _1.VariablesConstantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aquí escribo el código fuente
+            string nombre="Juan Manuel";
+            Console.WriteLine("Bienvenido "+ nombre + " al curso de Fundamentos de Programación");
+        }
+    }
+}
