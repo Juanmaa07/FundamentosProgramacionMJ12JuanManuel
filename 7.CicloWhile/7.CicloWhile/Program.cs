9e9282e268c24f368c7c65def78388308122062a
Juanmaa07 <juan.castanoz@upb.edu.co>
2026-08-25 13:31:21 -0500

    While y Contadores

---
+++ 7.CicloWhile/7.CicloWhile/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _7.CicloWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+/*            //1. Escribir un algoritmo que permita generar los primeros 5 n+umeros enteros positivos y realizar y mostrar su suma.
+            int contador = 1;
+            int acumulador = 1;
+
+            while (contador < 5)
+            {
+                contador ++;
+                acumulador=acumulador+contador;
+            }
+
+            Console.WriteLine($"La suma de los cinco primeros números es: {acumulador}");*/
+
+            //2. Crear un algoritmo que permita calcular la factorial de un número dado por el usuario. (5! = 1*2*3*4*5)
+
+            int contador = 1;
+            int acumulador = 1;
+            int numero;
+            Console.WriteLine("Ingrese el número.");
+            numero = int.Parse(Console.ReadLine());
+
+            while (contador <= numero)
+            {
+                acumulador = acumulador * contador;
+                contador++;
+            }
+            Console.WriteLine($"La factorial del número dado es: {acumulador}");
+        }
+    }
+}
