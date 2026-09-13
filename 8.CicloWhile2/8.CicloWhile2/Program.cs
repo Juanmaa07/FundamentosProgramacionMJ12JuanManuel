bb049a1e8cca9c6013e3fa91c65fe1c7d69364c3
Juanmaa07 <juan.castanoz@upb.edu.co>
2026-09-13 17:28:56 -0500

    Trabajo 8.2 y trabajo 9

---
+++ 8.CicloWhile2/8.CicloWhile2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _8.CicloWhile2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Realizar un algoritmo que pida números enteros positivos y los sume, hasta que se ingrese un número entero negativo. Se debe mostrar por pantalla el total de la suma de los números ingresados
+
+            int sumaEnteros = 0;
+            int numero = 0;
+            Console.WriteLine("Ingrese un número para sumar:");
+            numero=int.Parse(Console.ReadLine());
+
+            while(numero >= 0)
+            {
+                sumaEnteros += numero;
+                Console.WriteLine("Ingrese un número para sumar:");
+                numero = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"La suma de los números ingresados es {sumaEnteros}");
+        }
+    }
+}
---
+++ 9.CicloWhile3/9.CicloWhile3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _9.CicloWhile3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Realizar un algoritmo que le pida al usuario ingresar por teclado números enteros al azar. Al usuario le corresponde ingresar la cantidad de números que va a introducir. El algoritmo debe escribir en pantalla: 1. La cantidad de números introducidos que son mayores que 0, 2. La cantidad de números introducidos que son menores que 0, 3. La cantidad de números iguales a 0.
+
+/*            int cantidadNumeros = 0;
+            int contador = 0;
+            int numero = 0;
+            int contadorCeros = 0;
+            int contadorPositivos = 0;
+            int contadorNegativos = 0;
+            Console.WriteLine("Cuál es la cantidad de números a evaluar");
+            cantidadNumeros=int.Parse(Console.ReadLine());
+
+            while (contador < cantidadNumeros)
+            {
+                contador++;
+                Console.WriteLine("Ingrese el número a evaluar");
+                numero=int.Parse(Console.ReadLine());
+
+                if (numero == 0)
+                {
+                    contadorCeros++;
+                }
+                else
+                {
+                    if (numero > 0)
+                    { 
+                        contadorPositivos++;
+                    }
+                    else
+                    {
+                        contadorNegativos++;
+                    }
+                }
+            }
+            Console.WriteLine($"De los números ingresados hay {contadorPositivos} números positivos, hay {contadorNegativos} números negativos y hay {contadorCeros} números iguales a 0.");*/
+
+            //Escribir un algoritmo que cuente cuántos números pares hay en un rango de números del 1 al número n. (Usar el operador mod en C#).
+
+
+        }
+    }
+}
